147399045fbee354eb9d4edc276f39b30994c5ec
류별리 <byeoulri.ryu@hancom.com>
2023-10-17 16:50:45 +0900

    임금 지급 테스트 코드 1차 작성

--- src/main/java/org/example/transaction/pay/PayCheck.java
+++ src/main/java/org/example/transaction/pay/PayCheck.java
@@ -1,11 +1,12 @@
 package org.example.transaction.pay;
 
-import lombok.*;
-import org.example.PayrollDatabase;
-import org.example.entity.Employee;
-import org.example.transaction.Transaction;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -16,8 +17,21 @@ public class PayCheck {
     private double grossDay;
     private double deductions;
     private double netPay;
+    
+    @Builder.Default
+    private Map<String, String> fieldMap = new HashMap<>();
 
-    public String getDisposition() {
-        return "";
+    public String getField(String fieldName) {
+        return fieldMap.get(fieldName);
+    }
+    
+    public double getCrossPay() {
+        //TODO: 메서드 구현 필요
+        return 0.0;
+    }
+    
+    public Date getPayPeriodEndDate() {
+        //TODO: 메서드 구현 필요
+        return new Date();
     }
 }
--- src/test/java/pay/PayEmployeeTest.java
+++ src/test/java/pay/PayEmployeeTest.java
@@ -3,18 +3,19 @@ package pay;
 import org.example.PayrollDatabase;
 import org.example.addEmployee.AddHourlyEmployee;
 import org.example.addEmployee.AddSalariedEmployee;
-import org.example.entity.Employee;
-import org.example.transaction.change.ChangeAddressTransaction;
-import org.example.transaction.change.ChangeNameTransaction;
+import org.example.transaction.add.ServiceChargeTransaction;
+import org.example.transaction.add.TimeCardTransaction;
+import org.example.transaction.change.ChangeMemberTransaction;
 import org.example.transaction.pay.PayCheck;
 import org.example.transaction.pay.PaydayTransaction;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PayEmployeeTest {
 
@@ -32,79 +33,351 @@ public class PayEmployeeTest {
     public void testPaySingleSalariedEmployee() throws Exception {
 
         AddSalariedEmployee t = new AddSalariedEmployee(empId, name, address, 1000.00);
-        t.excute();
+        t.execute();
 
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2001, 10, 30);
+        calendar.set(2001, Calendar.NOVEMBER, 30);
         Date payDate = calendar.getTime();
 
         PaydayTransaction pt = PaydayTransaction.builder()
                 .payDate(payDate)
                 .build();
-        pt.excute();
+        pt.execute();
 
         PayCheck pc = pt.getPayCheck(empId);
-        Assertions.assertNotNull(pc);
-        Assertions.assertEquals(pc.getPayDate(), payDate);
-        //Assertions.assertEquals("Hold", pc.getDisposition());
-        Assertions.assertEquals(0.0, pc.getDeductions(), .001);
-        Assertions.assertEquals(1000.00, pc.getNetPay(), .001);
+        assertNotNull(pc);
+        assertEquals(pc.getPayDate(), payDate);
+        assertEquals("Hold", pc.getField("Disposition"));
+        assertEquals(0.0, pc.getDeductions(), .001);
+        assertEquals(1000.00, pc.getNetPay(), .001);
 
     }
 
     @Test
     @Order(2)
     public void testPaySingleHourlyEmployeeNoTimeCards() throws Exception {
-
-
+        int empId = 1;
+        AddSalariedEmployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+        t.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 30);
+        Date payDate = calendar.getTime();
+    
+        PaydayTransaction pt = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        pt.execute();
+        
+        PayCheck pc = pt.getPayCheck(empId);
+        assertNull(pc);
     }
 
     @Test
     @Order(3)
     public void testPaySingleHourlyEmployeeOneTimeCard() throws Exception {
-
-
+        int empId = 2;
+        AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bill", "Home", 15.25);
+        t.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = calendar.getTime();
+    
+        //TODO: HourlyClassification 내 long date 처리 수정 필요
+        TimeCardTransaction tc = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(2.0)
+                .build();
+        tc.execute();
+        
+        PaydayTransaction pt = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        pt.execute();
+    
+        PayCheck pc = pt.getPayCheck(empId);
+        assertNull(pc);
+    
+        validateHourlyPaycheck(pt, empId, payDate, (8 + 1.5) * 15.25);
     }
+    
+    private void validateHourlyPaycheck(PaydayTransaction paydayTransaction, int empId, Date payDate, double pay) {
+        PayCheck payCheck = paydayTransaction.getPayCheck(empId);
+        assertEquals(payDate, payCheck.getPayDate());
+        assertEquals(pay, payCheck.getCrossPay(), .001);
+        assertEquals("Hold", payCheck.getField("Disposition"));
+        assertEquals(0.0, payCheck.getDeductions(), .001);
+        assertEquals(1000.00, payCheck.getNetPay(), .001);
+    }
+    
+    @Test
+    public void TestPaySingleHourlyEmployeeOvertimeOneTimeCard() throws Exception {
+        int empId = 2;
+        AddHourlyEmployee addHourlyEmployee = new AddHourlyEmployee(empId, "Bill", "Home", 15.25);
+        addHourlyEmployee.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = calendar.getTime();
+        
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(9.0)
+                .build();
+        timeCardTransaction.execute();
+        
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+        
+        validateHourlyPaycheck(paydayTransaction, empId, payDate, (8 + 1.5) * 15.25);
+    }
+    
+    
 
     @Test
     @Order(4)
     public void testPaySingleHourlyEmployeeOnWrongDate() throws Exception {
-
+        int empId = 2;
+        AddHourlyEmployee addHourlyEmployee = new AddHourlyEmployee(empId, "Bill", "Home", 15.25);
+        addHourlyEmployee.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 8);
+        Date payDate = calendar.getTime();
+    
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(9.0)
+                .build();
+        timeCardTransaction.execute();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+    
+        PayCheck payCheck = paydayTransaction.getPayCheck(empId);
+        assertNull(payCheck);
 
     }
 
     @Test
     @Order(5)
     public void testPaySingleHourlyEmployeeTwoTimeCards() throws Exception {
-
-
+        int empId = 2;
+        AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bill", "Home", 15.25);
+        t.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = calendar.getTime();
+    
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(2.0)
+                .build();
+        timeCardTransaction.execute();
+    
+        calendar.set(2001, Calendar.NOVEMBER, 8);
+        Date payDate2 = calendar.getTime();
+    
+        TimeCardTransaction timeCardTransaction2 = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate2.getTime())
+                .hours(5.0)
+                .build();
+        timeCardTransaction2.execute();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+    
+        PayCheck pc = paydayTransaction.getPayCheck(empId);
+        validateHourlyPaycheck(paydayTransaction, empId, payDate, 7 * 15.25);
     }
+    
+    
 
     @Test
     @Order(6)
     public void testPaySingleHourlyEmployeeWithTimeCardsSpanningTwoPayPeriods() throws Exception {
-
+        int empId = 2;
+        AddHourlyEmployee addHourlyEmployee = new AddHourlyEmployee(empId, "Bill", "Home", 15.25);
+        addHourlyEmployee.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = calendar.getTime();
+    
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(2.0)
+                .build();
+        timeCardTransaction.execute();
+        
+        calendar.set(2001, Calendar.NOVEMBER, 2);
+        Date dateInPreviousPayPeriod = calendar.getTime();
+    
+        TimeCardTransaction timeCardTransaction2 = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(dateInPreviousPayPeriod.getTime())
+                .hours(5.0)
+                .build();
+        timeCardTransaction2.execute();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+    
+        validateHourlyPaycheck(paydayTransaction, empId, payDate, 2 * 15.25);
 
     }
 
     @Test
     @Order(7)
     public void testSalariedUnionMemberDues() throws Exception {
+        int empId = 1;
+        AddSalariedEmployee addSalariedEmployee = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+        addSalariedEmployee.execute();
+    
+        int memberId = 7734;
+        ChangeMemberTransaction changeMemberTransaction = new ChangeMemberTransaction(empId, memberId, 9.42);
+        changeMemberTransaction.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 30);
+        Date payDate = calendar.getTime();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
 
-
+        double consumerValue = 2.0;
+        validatePayCheck(paydayTransaction, empId, payDate, 1000.0 - consumerValue);
     }
-
+    
+    private void validatePayCheck(PaydayTransaction paydayTransaction, int empId, Date payDate, double pay) {
+        PayCheck payCheck = paydayTransaction.getPayCheck(empId);
+        assertEquals(payDate, payCheck.getPayDate());
+        assertEquals(pay, payCheck.getCrossPay(), .001);
+        assertEquals("Hold", payCheck.getField("Disposition"));
+        assertEquals(0.0, payCheck.getDeductions(), .001);
+        assertEquals(1000.00, payCheck.getNetPay(), .001);
+    }
+    
+    
     @Test
     @Order(8)
     public void testHourlyUnionMemberServiceCharge() throws Exception {
-
+        int empId = 1;
+        AddSalariedEmployee addSalariedEmployee = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+        addSalariedEmployee.execute();
+    
+        int memberId = 7734;
+        ChangeMemberTransaction changeMemberTransaction = new ChangeMemberTransaction(empId, memberId, 9.42);
+        changeMemberTransaction.execute();
+    
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = calendar.getTime();
+    
+        ServiceChargeTransaction serviceChargeTransaction = ServiceChargeTransaction.builder()
+                .memberId(memberId)
+                .date(payDate.getTime())
+                .amount(19.42)
+                .build();
+        serviceChargeTransaction.execute();
+    
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(8.0)
+                .build();
+        timeCardTransaction.execute();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+    
+        PayCheck payCheck = paydayTransaction.getPayCheck(empId);
+        assertEquals(payDate, payCheck.getPayPeriodEndDate());
+        assertEquals(8 * 15.24, payCheck.getCrossPay(), .001);
+        assertEquals("Hold", payCheck.getField("Disposition"));
+        assertEquals(9.42 + 19.42, payCheck.getDeductions(), .001);
+        assertEquals((8 * 15.24) - (9.42 + 19.42), payCheck.getNetPay(), .001);
 
     }
 
     @Test
     @Order(9)
     public void testServiceChargesSpanningMultiplePayPeriods() throws Exception {
-
+        int empId = 1;
+        AddSalariedEmployee addSalariedEmployee = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+        addSalariedEmployee.execute();
+    
+        int memberId = 7734;
+        ChangeMemberTransaction changeMemberTransaction = new ChangeMemberTransaction(empId, memberId, 9.42);
+        changeMemberTransaction.execute();
+    
+        Calendar payCalendar = Calendar.getInstance();
+        payCalendar.set(2001, Calendar.NOVEMBER, 2);
+        Date earlyDate = payCalendar.getTime();
+        payCalendar.set(2001, Calendar.NOVEMBER, 9);
+        Date payDate = payCalendar.getTime();
+        payCalendar.set(2001, Calendar.NOVEMBER, 16);
+        Date lateDate = payCalendar.getTime();
+    
+        ServiceChargeTransaction serviceChargeTransaction = ServiceChargeTransaction.builder()
+                .memberId(memberId)
+                .date(payDate.getTime())
+                .amount(19.42)
+                .build();
+        serviceChargeTransaction.execute();
+    
+        ServiceChargeTransaction serviceEarlyChargeTransaction = ServiceChargeTransaction.builder()
+                .memberId(memberId)
+                .date(earlyDate.getTime())
+                .amount(100.00)
+                .build();
+        serviceEarlyChargeTransaction.execute();
+    
+        ServiceChargeTransaction serviceLateChargeTransaction = ServiceChargeTransaction.builder()
+                .memberId(memberId)
+                .date(lateDate.getTime())
+                .amount(200.00)
+                .build();
+        serviceLateChargeTransaction.execute();
+    
+        TimeCardTransaction timeCardTransaction = TimeCardTransaction.builder()
+                .empId(empId)
+                .date(payDate.getTime())
+                .hours(8.0)
+                .build();
+        timeCardTransaction.execute();
+    
+        PaydayTransaction paydayTransaction = PaydayTransaction.builder()
+                .payDate(payDate)
+                .build();
+        paydayTransaction.execute();
+    
+        PayCheck payCheck  = paydayTransaction.getPayCheck(empId);
+        assertEquals(payDate, payCheck.getPayPeriodEndDate());
+        assertEquals(8 * 15.24, payCheck.getCrossPay(), .001);
+        assertEquals("Hold", payCheck.getField("Disposition"));
+        assertEquals(9.42 + 19.42, payCheck.getDeductions(), .001);
+        assertEquals((8 * 15.24) - (9.42 + 19.42), payCheck.getNetPay(), .001);
 
     }
 
